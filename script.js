f7e2e06cba4e18c0f8837592babb4860d3c30d53
KamilStepien <43994674+KamilStepien@users.noreply.github.com>
2023-07-15 10:02:07 +0200

    d

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date(2023, 6, 16).getTime();
+let countDownDate = new Date(2023, 6, 15).getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function() {
